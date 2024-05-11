f40a6d270ab8551affeeaa48caf98ee92ef4cdb6
Christoph <c.wagner.germany@gmail.com>
2024-05-11 16:43:11 +0200

    Dumbbells

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,18 +1,37 @@
 class Product {
-    constructor(title, image, desc, price) {
-        this.title = title;
-        this.imageUrl = image;
-        this.description = desc;
-        this.price = price;
-    }
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
+  }
 }
 
 class ProductItem {
-    constructor(product) {
-        this.product = product;
-    }
+  constructor(product) {
+    this.product = product;
+  }
 
-    render() {
+  render() {
+    const prodEl = document.createElement('li');
+    prodEl.className = 'product-item';
+    prodEl.innerHTML = `
+            <div>
+                <img src="${this.product.imageUrl}" alt="${this.product.title}">
+                <div class="product-item__content">
+                    <h2>${this.product.title}</h2>
+                    <h3>${this.product.price}</h3>
+                    <p>${this.product.description}</p>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        `;
+    return prodEl;
+  }
+}
 
-    }
+class ProductList {
+  products = [
+    new Product('Dumbbells', '', 'State of the art dumbbells', 49.99),
+  ];
 }
