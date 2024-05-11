a3439066ad3c59fa52fd7e71ff34a446c439ce05
Christoph <c.wagner.germany@gmail.com>
2024-05-11 20:10:28 +0200

    bootstrap5

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -51,13 +51,18 @@ class ProductList {
 
   render() {
     const renderHook = document.querySelector('body');
-    const prodList = document.createElement('div');
+    // const prodList = document.createElement('div');
+    // for (const prod of this.products) {
+    //   const productItem = new ProductItem(prod);
+    //   const prodEl = productItem.render();
+    //   prodList.append(prodEl);
+    // }
+    // renderHook.append(prodList);
     for (const prod of this.products) {
       const productItem = new ProductItem(prod);
       const prodEl = productItem.render();
-      prodList.append(prodEl);
+      renderHook.append(prodEl);
     }
-    renderHook.append(prodList);
   }
 }
 
