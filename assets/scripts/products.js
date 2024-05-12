35e4eb11e12c6fafc5ed1e42aa4d9306c67bbb88
Christoph <c.wagner.germany@gmail.com>
2024-05-12 11:51:54 +0200

    12May4

--- assets/scripts/products.js
+++ assets/scripts/products.js
@@ -1,25 +1,25 @@
 class Product {
-    constructor(title, image, desc, price) {
-      this.title = title;
-      this.imageUrl = image;
-      this.description = desc;
-      this.price = price;
-    }
+  constructor(title, image, desc, price) {
+    this.title = title;
+    this.imageUrl = image;
+    this.description = desc;
+    this.price = price;
   }
-  
-  const PRODUCTS = [
-    new Product(
-      'Dumbbells',
-      './assets/img/fitness-5013986_640.jpg',
-      'State of the Art Dumbbells',
-      49.99
-    ),
-    new Product(
-      'Barbell',
-      './assets/img/gym-592899_640.jpg',
-      'High Quality Barbell',
-      99.99
-    ),
-  ];
+}
+
+const PRODUCTS = [
+  new Product(
+    'Dumbbells',
+    './assets/img/fitness-5013986_640.jpg',
+    'State of the Art Dumbbells',
+    49.99
+  ),
+  new Product(
+    'Barbell',
+    './assets/img/gym-592899_640.jpg',
+    'High Quality Barbell',
+    99.99
+  ),
+];
 
 export { Product, PRODUCTS };
